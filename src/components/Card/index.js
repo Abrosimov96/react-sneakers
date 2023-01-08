8cca3b2f253af989687e5b8c8f29e1de0f0ab451
Abrosimov96 <Abrosimov0423@gmail.com>
2023-01-08 16:05:04 +0200

    Работа с пропсами + CSS модуль

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,9 @@
-function Card() {
+import styles from './Card.module.scss';
+
+function Card(props) {
   return (
-    <div className='card'>
-      <div className='favorite'>
+    <div className={styles.card}>
+      <div className={styles.favorite}>
         <img
           src='/img/heart-unliked.svg'
           alt=''
@@ -10,14 +12,14 @@ function Card() {
       <img
         width={133}
         height={112}
-        src='/img/sneakers/1.jpg'
+        src={props.imageUrl}
         alt='Sneaker'
       />
-      <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+      <h5>{props.title}</h5>
       <div className='d-flex justify-between align-center'>
         <div className='d-flex flex-column'>
           <span>Цена:</span>
-          <b>12 999 руб.</b>
+          <b>{props.price} руб.</b>
         </div>
         <button className='button'>
           <img
